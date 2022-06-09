48e4426595b0aece2e9d6499647110a8c736890a
xcixor <pndungu54@gmail.com>
2022-06-10 01:32:06 +0300

    feat(ui): add a template for landing page

--- src/app/presentation/EntryPointController.java
+++ src/app/presentation/EntryPointController.java
@@ -1,4 +1,35 @@
 package app.presentation;
 
-public class EntryPointController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.kordamp.bootstrapfx.BootstrapFX;
+
+public class EntryPointController implements Initializable {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private Button settingsBtn;
+
+    @FXML
+    private Button homeBtn;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        borderPane.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
+//        initializeButtonIcons();
+    }
+
+    private void initializeButtonIcons() {
+
+    }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,12 @@
 module SchoolManagementSystem {
     requires javafx.fxml;
     requires javafx.controls;
+    requires org.kordamp.bootstrapfx.core;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.fontawesome;
 
     opens app to javafx.graphics, javafx.fxml;
+    opens app.presentation to javafx.fxml;
     exports app.presentation to javafx.fxml;
 }
